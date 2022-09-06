510fa9cc396351f2e102833fd9081e7384932af7
Dr. Anandkumar Prakasam <root.anand@gmail.com>
2022-09-06 11:26:28 +0530

    Add files via upload

---
+++ 2.ado sql Demo2/Backup/ado sql Demo2/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+//for microsoft sql server
+using System.Data.SqlClient;
+
+namespace ado_sql_Demo2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //select using scalar
+            SqlConnection sqlconn = new SqlConnection();
+
+            // sqlconn.ConnectionString = "SERVER=DECOY-PC\\SQLEXPRESS; Database=Student; uid=root;pwd=data";
+            //sqlconn.ConnectionString = "SERVER=DECOY-PC\\SQLEXPRESS; Database=Student; trusted_connection=true";
+            //  sqlconn.ConnectionString = "SERVER=.\\SQLEXPRESS; Database=Student; uid=root;pwd=data";
+            // sqlconn.ConnectionString = "SERVER=.\\SQLEXPRESS; Database=Student; Integrated Security=true";
+            sqlconn.ConnectionString = "SERVER=DECOY-PC\\SQLEXPRESS; Database=Student; Integrated security=SSPI";
+            //Security Support Provider Interface- an API (.dll file) that is used to provide Windows Authentication
+            try
+            {
+                sqlconn.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Connection Failed");
+
+            }
+            if (sqlconn.State == ConnectionState.Open)
+            {
+                this.Text = "welcome to ADO.Net";
+
+
+                //select using scalar
+                SqlCommand sqlcomm = new SqlCommand();
+                sqlcomm.Connection = sqlconn;
+                //sqlcomm.CommandText = "select name from student where Studentid='1'";
+               
+               sqlcomm.CommandText = "select name from student where Studentid='" + textBox3.Text + "'";
+
+                MessageBox.Show(sqlcomm.CommandText);
+                MessageBox.Show(Convert.ToString(sqlcomm.ExecuteScalar()));
+
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlconn = new SqlConnection();
+
+            // sqlconn.ConnectionString = "SERVER=DECOY-PC\\SQLEXPRESS; Database=Student; uid=root;pwd=data";
+            //  sqlconn.ConnectionString = "SERVER=.\\SQLEXPRESS; Database=Student; uid=root;pwd=data";
+            sqlconn.ConnectionString = "SERVER=.\\SQLEXPRESS; Database=Student; Integrated Security=true";
+           // sqlconn.ConnectionString = "SERVER=.\\SQLEXPRESS; Database=Student; Trusted_connection=true";
+            try
+            {
+                sqlconn.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Connection Failed");
+
+            }
+            if (sqlconn.State == ConnectionState.Open)
+            {
+                this.Text = "welcome to ADO.Net";
+                SqlCommand sqlcomm = new SqlCommand();
+                sqlcomm.Connection = sqlconn;
+//insert using nonquery
+                //sqlcomm.CommandText = "insert into student values('1','anand')";
+                
+                              
+                
+                sqlcomm.CommandText = "insert into Student values('" + textBox1.Text + "','" + textBox2.Text + "')";
+                MessageBox.Show(sqlcomm.CommandText);
+                sqlcomm.ExecuteNonQuery();
+
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlconn = new SqlConnection();
+
+            // sqlconn.ConnectionString = "SERVER=DECOY-PC\\SQLEXPRESS; Database=Student; uid=root;pwd=data";
+            //  sqlconn.ConnectionString = "SERVER=.\\SQLEXPRESS; Database=Student; uid=root;pwd=data";
+            // sqlconn.ConnectionString = "SERVER=.\\SQLEXPRESS; Database=Student; Integrated Security=true";
+            //sqlconn.ConnectionString = "SERVER=.\\SQLEXPRESS; Database=Student; Trusted_connection=true";
+            sqlconn.ConnectionString = "SERVER=.\\SQLEXPRESS; Database=Student; Integrated Security=SSPI";
+            try
+            {
+                sqlconn.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Connection Failed");
+
+            }
+            if (sqlconn.State == ConnectionState.Open)
+            {
+                this.Text = "welcome to ADO.Net";
+                SqlCommand sqlcomm = new SqlCommand();
+                sqlcomm.Connection = sqlconn;
+
+                //delete using non query
+                sqlcomm.CommandText = "delete from Student where Studentid='" + textBox4.Text + "'";
+
+                MessageBox.Show(sqlcomm.CommandText);
+                sqlcomm.ExecuteNonQuery();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlconn = new SqlConnection();
+
+            // sqlconn.ConnectionString = "SERVER=DECOY-PC\\SQLEXPRESS; Database=Student; uid=root;pwd=data";
+            //  sqlconn.ConnectionString = "SERVER=.\\SQLEXPRESS; Database=Student; uid=root;pwd=data";
+            // sqlconn.ConnectionString = "SERVER=.\\SQLEXPRESS; Database=Student; Integrated Security=true";
+            //sqlconn.ConnectionString = "SERVER=.\\SQLEXPRESS; Database=Student; Trusted_connection=true";
+            sqlconn.ConnectionString = "SERVER=.\\SQLEXPRESS; Database=Student; Integrated Security=SSPI";
+            try
+            {
+                sqlconn.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Connection Failed");
+
+            }
+            if (sqlconn.State == ConnectionState.Open)
+            {
+                this.Text = "welcome to ADO.Net";
+                SqlCommand sqlcomm = new SqlCommand();
+                sqlcomm.Connection = sqlconn;
+
+                //update name using non query
+                sqlcomm.CommandText = "update Student set name='" + textBox6.Text + "' where Studentid='" + textBox5.Text + "'";
+
+                MessageBox.Show(sqlcomm.CommandText);
+                sqlcomm.ExecuteNonQuery();
+            }
+        }
+    }
+}
+ 
+
---
+++ 3.adosqldatareader1/adosqldatareader1/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace adosqldatareader1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlconn = new SqlConnection();
+            sqlconn.ConnectionString = "SERVER=.\\; DATABASE= pubs; Integrated Security=true";
+            try
+            {
+                sqlconn.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            if (sqlconn.State == ConnectionState.Open)
+            {
+                //MessageBox.Show("Welcome!!!");
+            }
+            SqlCommand sqlcomm = new SqlCommand();
+            sqlcomm.Connection=sqlconn;
+           //sqlcomm.CommandText = "select au_id from authors";
+          //sqlcomm.CommandText = "select au_id, au_fname, au_lname from authors";
+         sqlcomm.CommandText = "select * from authors";
+            SqlDataReader dr;
+            dr = sqlcomm.ExecuteReader();
+            listBox1.Items.Clear();
+
+            while (dr.Read())
+            {
+
+                //listBox1.Items.Add(dr.GetString(2));
+
+
+                //listBox1.Items.Add(dr.GetString(0) + "  " + dr.GetString(1) + "  " + dr.GetString(2));
+
+                //listBox1.Items.Add(dr.GetString(0));
+                //listBox1.Items.Add(dr.GetString(2) + " " + dr.GetString(1));
+
+                listBox1.Items.Add("author id : " + dr.GetString(0));
+                listBox1.Items.Add("First name : " + dr.GetString(2));
+                listBox1.Items.Add("Last name : " + dr.GetString(1));
+                listBox1.Items.Add("");
+
+            }
+            dr.Close();
+        }
+    }
+}
